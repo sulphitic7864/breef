e9d3d4ec053550dd07e31c45cff282c736c73d00
sulphitic7864 <wqasimg4@gmail.com>
2025-12-05 12:14:43 +0500

    Home is updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -433,18 +433,23 @@ export default function Home() {
             </h4>
             <TwitsCarousel cards={twitsData} />
           </div>
-          <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      sfsfsf
     </div>
     <div class="carousel-item">
-      ffffffffffff
     </div>
     <div class="carousel-item">
-      wwww
     </div>
   </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
         </div>
       </section>
